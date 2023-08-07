91e6ff00b636492a4efd5c32aa26f3a3c53fd137
bbsemih <semihberkayozturk@Semihs-MacBook-Pro.local>
2023-08-07 12:37:21 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserModule } from './modules/user/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './core/database/database.module';
+import { GuardModule } from './modules/guard/guard.module';
+import { MessageModule } from './modules/message/message.module';
+import { ServerModule } from './modules/server/server.module';
+import { UserService } from './modules/user/user.service';
+
+@Module({
+  imports: [UserModule, AuthModule, DatabaseModule, GuardModule, ServerModule, MessageModule],
+  controllers: [AppController],
+  providers: [AppService, UserService],
+})
+export class AppModule {}
---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,39 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+@Table
+export class User extends Model<User> {
+    //id column
+    @Column({
+        type: DataType.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+    })
+    id: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    username: string;
+
+    @Column({
+        type: DataType.STRING,
+        unique: true,
+        allowNull: false,
+    })
+    email: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    password: string;
+
+    @Column({
+        type: DataType.ENUM,
+        values: ['male', 'female'],
+        allowNull: false,
+    })
+    gender: string;
+}
