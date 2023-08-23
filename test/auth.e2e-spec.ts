0add9507ffdd126a7e35d4802f0567c6d8010d87
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-08-23 21:13:38 +0300

    start to implement auth e2e test

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication Module (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  //TODO: Add more tests
+  it('handles a signup request', () => {
+    const email = 'semih.im@gmail.com';
+    return request(app.getHttpServer())
+      .post('/auht/signup')
+      .send({ email, password: 'semihb' })
+      .expect(201)
+      .then(res => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
